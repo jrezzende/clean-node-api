0d032b31364141f4ef06eb5fce8e1c022592bc83
João Rezende <jmprezende1996@gmail.com>
2021-02-15 17:41:12 -0300

    feat: ensure 400 is thrown is no password fields are provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 
 export class SignupController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
